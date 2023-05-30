8324f392b7b04b5477d7497117f1b20a3b57f7b0
SebastianSL10 <119981305+SebastianSL10@users.noreply.github.com>
2023-05-30 09:26:51 -0500

    React
    React_FirstProyect

---
+++ src/Product.js
@@ -0,0 +1,13 @@
+function Products() {
+    return <div>
+    <h1>Productos Spider-Man</h1>
+    </div>
+}
+export function Nom() {
+    return <nav>
+    <h2>Su papá</h2>
+    </nav>
+    
+}
+
+export default Products
---
+++ src/greeting.js
@@ -0,0 +1,8 @@
+export function Greeting() {
+    return <h1>Componente de React</h1>
+}
+
+export function Usercard() {
+    return <h1>User card de mi Tia</h1>
+    
+}
